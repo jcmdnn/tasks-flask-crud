868a047ac4ea1496c9eaae36ce377ac4a9017107
joaocarlito <joaocarlito@proton.me>
2024-10-30 20:53:53 -0300

    created route get_tasks and correction return data title

--- app.py
+++ app.py
@@ -10,10 +10,19 @@ task_id_control = 1
 def create_task():
     global task_id_control
     data = request.get_json()
-    new_task = Task(id=task_id_control, title=["title"], description=data.get("description", ""))
+    new_task = Task(id=task_id_control, title=data["title"], description=data.get("description", ""))
     task_id_control += 1
     tasks.append(new_task)
     return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    output = {
+            "tasks": task_list,
+            "total_tasks": len(task_list)
+            }
+    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
