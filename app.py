fc010977422ab2e708faa357b548fd3d13529d01
joaocarlito <joaocarlito@proton.me>
2024-10-30 21:14:20 -0300

    created route get_task

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 tasks = []
 task_id_control = 1
 
+#create task
 @app.route('/tasks', methods=['POST'])
 def create_task():
     global task_id_control
@@ -15,6 +16,7 @@ def create_task():
     tasks.append(new_task)
     return jsonify({"message": "Nova tarefa criada com sucesso"})
 
+#list all tasks
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
     task_list = [task.to_dict() for task in tasks]
@@ -23,6 +25,15 @@ def get_tasks():
             "total_tasks": len(task_list)
             }
     return jsonify(output)
+
+# list only one task
+@app.route('/tasks/<int:id_task>', methods=['GET'])
+def get_task(id_task):
+    for task in tasks:
+        if task.id == id_task:
+            return jsonify(task.to_dict())
+    
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
